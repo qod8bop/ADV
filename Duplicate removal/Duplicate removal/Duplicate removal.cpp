1650bcf9b33e11156284d7c3c7b5398fee330989
qod8bop <turcenkovtimofej99228@gmail.com>
2025-09-28 11:38:38 +0300

    NetologiyaKontoraPidorasov

---
+++ Duplicate removal/Duplicate removal/Duplicate removal.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <list>
+
+int main()
+{
+	std::list<int> zalupa;
+	int a;
+
+	std::cout << "Вводите числа, когда надоест, введите 0\n";
+
+	while(true)
+	{
+		std::cout << "Число: ";
+		std::cin >> a;
+		std::cout << std::endl;
+
+		if (a == 0) { break; }
+
+		zalupa.push_front(a);
+
+	}
+
+	zalupa.unique();
+	zalupa.sort();
+
+	for(auto it = zalupa.begin(); it!=zalupa.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
+
+
+	return 0;
+}
---
+++ Symbofreq/Symbofreq/Symbofreq.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+
+int main()
+{
+	std::string line = "Hello World!!";	
+	int size = line.length();
+
+	std::map<char, int> map;
+
+
+	for(int i=0; i++; i<size)
+	{
+
+		std::cout << line[i] << std::endl; // Я понятия не имею, почему эта строчка просто игнорируется
+		map[line[i]] = 1;
+
+		for(int j=0; j ++; j<size)
+		{
+			if (line[j] == line[i]) { map[line[i]]++; }
+		}
+
+		std::cout << map[line[i]];
+	}
+	
+
+
+	return 0;
+}
