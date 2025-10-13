4344a439f2cc6dc02d249600ccde6f29e06ba28c
qod8bop <turcenkovtimofej99228@gmail.com>
2025-10-14 00:28:30 +0300

    wgewegewrg

---
+++ Printgovna/Printgovna/Printgovna.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+#include <algorithm>
+
+template<typename C>
+void  print_container (C cont)
+{
+	std::C <std::string>::iterator i = cont.begin(); 
+	while (i != cont.end()) 
+	{
+		std::cout << *i << std::endl; 
+		i++; 
+	}
+}
+
+int main()
+{
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+
+	print_container(test_set);
+	std::cout << std::endl;
+
+	print_container(test_list);
+	std::cout << std::endl;
+
+	print_container(test_vector);
+	std::cout << std::endl;
+
+	return 0;
+}
---
+++ Uniquepohui/Uniquepohui/Uniquepohui.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> v = { 1, 1, 2, 5, 6, 1, 2, 4 };
+	std::sort(v.begin(), v.end());
+	std::unique(v.begin(), v.end());
+	std::vector<int>::iterator i = v.begin(); 
+	while (i != v.end()) 
+	{
+		std::cout << *i << "  ";
+		i++; 
+	}
+	return 0;
+}
